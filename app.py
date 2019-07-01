aab5b7e36dacae4ceb2558c0934651f43fa9778a
VasilPanchuk <vasil.panchuk@gmail.com>
2019-07-01 19:34:13 +0300

    Add comments

--- app.py
+++ app.py
@@ -9,8 +9,21 @@ db = SQLAlchemy(app)
 @app.route('/')
 def home():
     from models import Post
-    posts = Post.query.all()
-    return render_template('all_posts.html', posts=posts)
+    post_objects = Post.query.all()
+    post_array = []
+    for p in post_objects:
+        post_array.append(
+            {
+                'id': p.id,
+                'title': p.title,
+                'text': p.text[:500],
+                'date_created': p.date_created,
+                'is_visible': p.is_visible,
+                'str_end': '>>>',
+            }
+        )
+
+    return render_template('all_posts.html', posts=post_array)
 
 @app.route('/add_post/', methods=['GET', 'POST'])
 def add_form():
@@ -29,6 +42,35 @@ def add_form():
         return render_template('add_post.html')
     else:
         return 'bad method', 400
+
+@app.route("/view_post/<p_id>")
+def view_post(p_id):
+    from models import Post, Comment
+    id = int(p_id)
+    post = Post.query.filter_by(id=id).first()
+    comments = Comment.query.filter_by(post_id=id)
+    p = {
+        'post': post,
+        'comments': comments
+    }
+    return render_template('view_post.html', post=p)
+
+@app.route("/add_comment/", methods=['POST'])
+def add_comment():
+    from models import Comment
+    from forms import CommentForm
+    if request.method == 'POST':
+        form = CommentForm(request.form)
+        if form.validate():
+            comment = Comment(**form.data)
+            db.session.add(comment)
+            db.session.commit()
+            return view_post(comment.post_id)
+        else:
+            return str(form.errors), 400
+    else:
+        return 'bad method', 400
+
 
 
 if __name__ == '__main__':
